fb24539f8847b7c788a6a168b2ba80bb4809f3b4
cassiomeira12 <cassiomeira_12@hotmail.com>
2020-03-12 13:22:48 -0300

    Adicionando novas rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import App from './App';
+import Releases from './container/Releases';
+import Backlog from './container/Backlog';
+import Sprints from './container/Sprints';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Router path='/' component={App}/>
+    <Router path='/releases' component={Releases}/>
+    <Router path='/backlog' component={Backlog}/>
+    <Router path='/sprints' component={Sprints}/>
+  </Router>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
